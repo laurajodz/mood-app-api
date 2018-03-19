5ad2d9d5c259ee6408b0b9247a7e0199c8961d58
Laura Jodz <laurajodz@gmail.com>
2018-03-19 07:09:06 -0700

    edit config file

--- config.js
+++ config.js
@@ -1,5 +1,3 @@
-// module.exports = {
-//     CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'http://localhost:3000'
-// };
+exports.CLIENT_ORIGIN = process.env.CLIENT_ORIGIN || 'http://localhost:3000'
 exports.DATABASE_URL = process.env.DATABASE_URL || 'mongodb://localhost/moods-app';
 exports.PORT = process.env.PORT || 8080;
